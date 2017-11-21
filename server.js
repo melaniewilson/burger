3745e8f9a47baac80134dbbe1d500fed8a464a9c
melanie <melaniew1357@gmail.com>
2017-11-20 19:19:38 -0800

    minor changes

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ var routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
 
 app.listen(PORT, function() {
-  console.log("Listening on PORT%s", PORT);
+  console.log("Listening on PORT", PORT);
 });
